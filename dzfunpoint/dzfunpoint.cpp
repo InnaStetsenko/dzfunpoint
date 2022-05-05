0fd245e54e529c7121a7c34175b8f5dfcac0e0b5
HP <HP@DESKTOP-4ON4CVO>
2022-05-06 00:06:53 +0300

    Добавьте файлы проекта.

---
+++ dzfunpoint/dzfunpoint.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <windows.h>
+//#include <conio.h>
+using namespace std;
+
+
+void rect(int width, int height, int* sq, int* p)
+{
+*sq = width * height;
+*p = (width + height) * 2;
+}int main()
+{
+	int w = 4;
+	int h = 3;
+	int s;
+	int p;
+	rect(w, h, &s, &p);
+}
+
+
